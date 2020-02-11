e13e1b5fd2bf5f80942fee007cc7cfe6543d9cc8
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-02-11 06:10:48 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot
-  phrase = "Squawk!"
+def parrot(Phrase)
+
   puts phrase
   phrase
 
 end
 
-parrot
+parrot("Squawk!")
